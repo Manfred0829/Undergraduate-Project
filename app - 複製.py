1e372b9e86e47f29f77b36349b2450108b610ac6
JohnnyDo0730 <johnny05190730@gmail.com>
2025-03-28 00:25:18 +0800

    Initial commit

---
+++ app - 複製.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    # 直接跳轉到主頁，無需驗證
+    return redirect(url_for('main'))
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
