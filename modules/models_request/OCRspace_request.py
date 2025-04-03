03799ceed1c5feca25d9d0b96ef13b71bb54c7f3
JohnnyDo0730 <johnny05190730@gmail.com>
2025-04-03 23:48:10 +0800

    新增OCRspace、更改相關聯的工具

---
+++ modules/models_request/OCRspace_request.py
@@ -0,0 +1,38 @@
+import concurrent.futures
+import time
+import json
+import requests
+
+from modules.module_template import LazySingleton
+
+class OCRspaceRequest(LazySingleton):
+    model = None
+    genai = None
+    OCRSPACE_API_TOKEN = None
+
+    def initialize(self):
+        """初始化方法"""
+        import config
+
+        # 獲取 Api key
+        self.OCRSPACE_API_TOKEN = config.get_env_variable("OCRSPACE_API_TOKEN")
+
+        # 設定已初始化
+        self._initialized = True
+
+
+    def generate_img_OCR(self, img_path, request_msg='What words are in the picture? Only give me the words.'):
+    
+        raise NotImplementedError("Using default Data, didn't implemented OCR process.")
+
+    
+    def merge_words_to_pages(OCR_result):
+        page_list = []
+        for page in OCR_result:
+            page_content = ""
+            for line in page:
+                page_content += "" + line["LineText"] + "\n"
+            
+            page_list.append(page_content)
+
+        return page_list
