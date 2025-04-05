391d89c7962622887aa25b9208455295e63599be
JohnnyDo0730 <johnny05190730@gmail.com>
2025-04-05 17:09:29 +0800

    完善processing_note之流程，目前僅缺少相似度查詢步驟

--- utils/main_processer.py
+++ utils/main_processer.py
@@ -4,13 +4,12 @@ from modules.models_local.EasyOCR_local import EasyOCRLocal
 from modules.models_request.OpenAI_request import OpenAIRequest
 
 
-
 def processing_note(img_path):
 
     # Img process
     EasyOCR = EasyOCRLocal()
     img_PIL = media.read_image_to_PIL(img_path)
-    cropped_images = EasyOCR.processing_lines_bounding_box(img_PIL,draw_result=False)
+    cropped_images = EasyOCR.processing_lines_bounding_box(img_PIL,draw_result=True)
 
     # OCR process
     OpenAI = OpenAIRequest()
@@ -22,4 +21,23 @@ def processing_note(img_path):
         #print(OCR_result_text)
         page_texts += OCR_result_text + "\n"
 
-    print(page_texts)
+    # LLM process
+    repaired_page = OpenAI.processing_notes_repair(page_texts)
+    notes_json = OpenAI.processing_notes_extract_notes(repaired_page)
+    print(repaired_page)
+    print(notes_json)
+
+    # embedding process
+    notes_embedding = OpenAI.processing_notes_embedding(notes_json)
+    for i in range(len(notes_embedding)):
+        notes_json[i]['Embedding'] = notes_embedding[i]
+
+    # simularity process
+    # temp mocking
+    for note in notes_json:
+        note['Keypoint_id'] = -1
+
+    # save result
+    notes_db = text.read_json(r"data\assembly_language\notes.json",default_content=[])
+    notes_db.extend(notes_json)
+    text.write_json(notes_db, r"data\assembly_language\notes.json")
--- utils/text_processer.py
+++ utils/text_processer.py
@@ -13,15 +13,23 @@ def write_json(data, relative_path):
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-def read_json(relative_path):
+def read_json(relative_path, default_content=None):
     """
-    從專案內的檔案讀取 JSON 物件。
+    從專案內的檔案讀取 JSON 物件。如果檔案不存在，則自動建立一個空的 JSON 檔案。
 
     :param relative_path: 以專案根目錄為基準的相對路徑
     :return: JSON 格式的物件
     """
     file_path = os.path.join(config.get_project_root(), relative_path)
     print(f"File path: {file_path}")
+
+    # 若檔案不存在，創建空 JSON 檔案
+    if not os.path.exists(file_path):
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'w', encoding='utf-8') as f:
+            json.dump(default_content, f, ensure_ascii=False, indent=4)
+
+    # 讀取 JSON 檔案內容
     with open(file_path, 'r', encoding='utf-8') as f:
         return json.load(f)
 
